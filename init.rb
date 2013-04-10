5db37715dcddc1526ba166c020762d8a1fdf21b1
Alex Mekhovov <alex.mehovov@gmail.com>
2013-04-10 18:40:20 +0300

    add: speakers, schedule

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ set :conferencies, {
     :location => 'Dnipropetrovsk',
     :limit => 70,
     :speakers => true,
-    :scheduled => false,
+    :scheduled => true,
     :vote => false,
     :report => false,
     :reg_open => false
