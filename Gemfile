1ed4c7028f5478cbb0c8740bd3e8097314447193
Serhii Herasymov <serhiiherasymov@gmail.com>
2015-03-23 16:18:27 +0200

    temporarilly disabling twitter-related features

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem 'haml'
-gem 'twitter'
+gem 'twitter', '4.0.0'
 gem 'thin'
--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'twitter'
+# require 'twitter'
 require 'haml'
 
 # require 'awesome_print'   # development mode
@@ -182,32 +182,32 @@ set :conferencies, {
 
 set :haml, :format => :html5
 
-Twitter.configure do |config|
-   config.consumer_key = 'iLHqIMRs3roKizOifRog'
-   config.consumer_secret = 'T4hETM7yRctra3nIUnmpU3Jex5BhFvHVnBT6uWYIOVA'
-   config.oauth_token = '524597088-lazizoKuBNsVDamGjUwQQ6r4q7mQJXzWeFkjMQSC'
-   config.oauth_token_secret = 'wHKMKnYUr79aUTDvVQs4XVPKdlNpOF8yJdttYUox05s'
-end
+# Twitter.configure do |config|
+#    config.consumer_key = 'iLHqIMRs3roKizOifRog'
+#    config.consumer_secret = 'T4hETM7yRctra3nIUnmpU3Jex5BhFvHVnBT6uWYIOVA'
+#    config.oauth_token = '524597088-lazizoKuBNsVDamGjUwQQ6r4q7mQJXzWeFkjMQSC'
+#    config.oauth_token_secret = 'wHKMKnYUr79aUTDvVQs4XVPKdlNpOF8yJdttYUox05s'
+# end
 
-get '/twitter' do; erb :twitter, :layout => false; end   # twitter
+# get '/twitter' do; erb :twitter, :layout => false; end   # twitter
 
 get '/google50839014440e56f6' do; erb :google50839014440e56f6; end   # GA
 
-get '/t' do
-    posts = {}
-  Twitter.search("@pacemaker_conf", :result_type => "recent").map do |status|
-      posts[status.created_at] = {:user => status.from_user_name, :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
-  end
-  Twitter.search("from:@pacemaker_conf", :result_type => "recent").map do |status|
-      posts[status.created_at] = {:user => status.from_user_name,  :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
-  end
-  Twitter.search("#pacemaker_conf", :result_type => "recent").map do |status|
-      posts[status.created_at] = {:user => status.from_user_name,  :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
-  end
-  
-  tweets = posts.keys.sort{|x, y| y <=> x}.inject({}){|tweets, key| tweets[key] = posts[key]; tweets}
-  tweets.to_json
-end
+# get '/t' do
+#     posts = {}
+#   Twitter.search("@pacemaker_conf", :result_type => "recent").map do |status|
+#       posts[status.created_at] = {:user => status.from_user_name, :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
+#   end
+#   Twitter.search("from:@pacemaker_conf", :result_type => "recent").map do |status|
+#       posts[status.created_at] = {:user => status.from_user_name,  :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
+#   end
+#   Twitter.search("#pacemaker_conf", :result_type => "recent").map do |status|
+#       posts[status.created_at] = {:user => status.from_user_name,  :author => status.from_user, :photo => status.profile_image_url, :msg => status.text}
+#   end
+#   
+#   tweets = posts.keys.sort{|x, y| y <=> x}.inject({}){|tweets, key| tweets[key] = posts[key]; tweets}
+#   tweets.to_json
+# end
 
 
 get '/' do
